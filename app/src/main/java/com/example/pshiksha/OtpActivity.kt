257d1daa87a22d6fc6c0214cd0f53c7909f4cabd
Chanpreet Singh <chanpreet3000@gmail.com>
2022-06-27 14:18:55 +0530

    Otp Activity UI

--- app/src/main/java/com/example/pshiksha/OtpActivity.kt
+++ app/src/main/java/com/example/pshiksha/OtpActivity.kt
@@ -2,14 +2,13 @@ package com.example.pshiksha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pshiksha.databinding.ActivityMainBinding
+import com.example.pshiksha.databinding.ActivityOtpBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+class OtpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
