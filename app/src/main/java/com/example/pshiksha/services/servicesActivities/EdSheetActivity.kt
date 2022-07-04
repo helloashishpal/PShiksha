58ea81c36f9562ffc55352dc063c1cf387086d6e
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2022-07-04 15:41:44 +0530

    service template

---
+++ app/src/main/java/com/example/pshiksha/services/servicesActivities/EdSheetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pshiksha.services.servicesActivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pshiksha.R
+
+class EdSheetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ed_sheet)
+    }
+}
